f9e5825ae13a9cdf0f0680e6059e53de6ada0cbe
karimprime <karimashraf000@gmail.com>
2025-11-08 21:27:25 +0200

    fix: 🐛 try again

--- apps/Fitness/src/app/app.routes.server.ts
+++ apps/Fitness/src/app/app.routes.server.ts
@@ -2,11 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**/main/home',
-    renderMode: RenderMode.Client,
-  },
-  {
     path: '**',
-    renderMode: RenderMode.Server,
+    renderMode: RenderMode.Client,
   },
 ];
