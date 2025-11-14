f7fe922c5e702635f325bfb4567e08d26e848429
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-11-14 21:44:18 +0200

    test: 💍 add spec for rendering image correct class names

--- apps/Fitness/src/app/features/pages/home/components/about-us/about-us.spec.ts
+++ apps/Fitness/src/app/features/pages/home/components/about-us/about-us.spec.ts
@@ -1,13 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SeoService } from './../../../../../core/services/seo/seo.service';
 import { AboutUs } from './about-us';
 
-describe('AboutUs', () => {
+describe.only('AboutUs', () => {
   let component: AboutUs;
   let fixture: ComponentFixture<AboutUs>;
+  let seoMock!: { update: jest.Mock };
 
   beforeEach(async () => {
+    seoMock = {
+      update: jest.fn(),
+    };
     await TestBed.configureTestingModule({
       imports: [AboutUs],
+      providers: [{ provide: SeoService, useValue: seoMock }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AboutUs);
@@ -17,5 +23,23 @@ describe('AboutUs', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(seoMock.update).toHaveBeenCalled();
+  });
+
+  it('should render trainer images with correct class names', () => {
+    const imgElements: NodeListOf<HTMLImageElement> =
+      fixture.nativeElement.querySelectorAll('img');
+
+    const expectedClasses = ['trainer-3', 'trainer-1', 'trainer-2'];
+
+    const trainerImgs = Array.from(imgElements).filter((img) =>
+      expectedClasses.includes(img.className)
+    );
+
+    expect(trainerImgs.length).toBe(expectedClasses.length);
+
+    trainerImgs.forEach((img, index) => {
+      expect(img.classList.contains(expectedClasses[index])).toBe(true);
+    });
   });
 });
