dd36816e14da663b4d020d3d97a8b0a8bfd97ec8
maher <maher.ahmed@engaz.ai>
2025-11-26 21:37:18 +0200

    fix:circular debendancey error

--- apps/Fitness/src/app/core/interceptors/auth.interceptor.ts
+++ apps/Fitness/src/app/core/interceptors/auth.interceptor.ts
@@ -1,32 +1,32 @@
 import {HttpInterceptorFn} from "@angular/common/http";
 import {inject} from "@angular/core";
 import {StorageKeys} from "../constants/storage.config";
-import {Translation} from "../services/translation/translation";
+import {DEFAULT_LANGUAGE} from "../constants/translation.constants";
 
 /**
  * HTTP Interceptor that adds authentication token and language headers to all requests
  */
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-    const translation = inject(Translation);
-
     // Get token from localStorage
     const token = typeof window !== "undefined" ? localStorage.getItem(StorageKeys.TOKEN) : null;
 
     // Get current language (ar or en)
-    const lang = translation.lang();
-
+    const lang =
+        typeof window !== "undefined"
+            ? localStorage.getItem(StorageKeys.LANGUAGE) || DEFAULT_LANGUAGE
+            : DEFAULT_LANGUAGE;
 
     // Clone the request and add headers
-    if (req.url.includes("www.themealdb.com") == false) {
-      const clonedRequest = req.clone({
-          setHeaders: {
-              ...(token && {token: token}),
-              lang: lang || "en",
-          },
-      });
+    // Skip external APIs and i18n files
+    if (req.url.includes("www.themealdb.com") == false && req.url.includes("/i18n/") == false) {
+        const clonedRequest = req.clone({
+            setHeaders: {
+                ...(token && {token: token}),
+                lang: lang,
+            },
+        });
 
-      return next(clonedRequest);
+        return next(clonedRequest);
     }
-    return next(req)
-
+    return next(req);
 };
