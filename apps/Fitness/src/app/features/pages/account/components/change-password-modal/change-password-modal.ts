3245c780574a7fae71d1e6b06c32d108e2937f10
Habiba <Habiba.em7@gmail.com>
2025-12-03 22:22:02 +0200

    fix: 🐛 using shared input 2.0

--- apps/Fitness/src/app/features/pages/account/components/change-password-modal/change-password-modal.ts
+++ apps/Fitness/src/app/features/pages/account/components/change-password-modal/change-password-modal.ts
@@ -2,22 +2,56 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DynamicDialogRef } from 'primeng/dynamicdialog';
 import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { FitnessInput } from '@fitness-app/fitness-form'; // Adjust path as needed
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-change-password-modal',
   standalone: true,
-  imports: [CommonModule, ButtonModule, InputTextModule, TranslateModule],
+  imports: [
+    CommonModule, 
+    ButtonModule, 
+    TranslateModule, 
+    FitnessInput,
+    ReactiveFormsModule
+  ],
   templateUrl: "./change-password-modal.html",
   styleUrl: "./change-password-modal.scss",
 })
 export class ChangePasswordModalComponent {
   private dialogRef = inject(DynamicDialogRef);
   protected translate = inject(TranslateService);
+  private fb = inject(FormBuilder);
 
-  submit(oldPassword: string, newPassword: string): void {
-    if (oldPassword && newPassword && newPassword.length >= 6) {
+  // submit(oldPassword: string, newPassword: string): void {
+  //   if (oldPassword && newPassword && newPassword.length >= 6) {
+  //     this.dialogRef.close({ oldPassword, newPassword });
+  //   }
+  // }
+
+  // close(): void {
+  //   this.dialogRef.close();
+  // }
+
+
+   passwordForm: FormGroup = this.fb.group({
+    oldPassword: ['', [Validators.required]],
+    newPassword: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  // Use type assertion to satisfy TypeScript
+  get oldPasswordControl(): AbstractControl {
+    return this.passwordForm.get('oldPassword')!;
+  }
+
+  get newPasswordControl(): AbstractControl {
+    return this.passwordForm.get('newPassword')!;
+  }
+
+  submit(): void {
+    if (this.passwordForm.valid) {
+      const { oldPassword, newPassword } = this.passwordForm.value;
       this.dialogRef.close({ oldPassword, newPassword });
     }
   }
