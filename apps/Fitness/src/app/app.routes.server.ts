7a6be519ca1757ed17f13f45f6b684a524e54850
Reham-Gomaa <rehamgomaa686@gmail.com>
2026-01-08 18:21:25 +0200

    change about to serverMode

--- apps/Fitness/src/app/app.routes.server.ts
+++ apps/Fitness/src/app/app.routes.server.ts
@@ -2,14 +2,6 @@ import {RenderMode, ServerRoute} from "@angular/ssr";
 
 export const serverRoutes: ServerRoute[] = [
     {
-        path: ":lang/main/about",
-        renderMode: RenderMode.Prerender,
-    },
-    {
-        path: ":lang/main/details",
-        renderMode: RenderMode.Client,
-    },
-    {
         path: "**",
         renderMode: RenderMode.Server,
     },
