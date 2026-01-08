82ee657ca222e1941dd08e7e70c50750d4f28d03
Reham-Gomaa <rehamgomaa686@gmail.com>
2026-01-08 14:40:27 +0200

    perf: ⚡️ change renderMode to server

--- apps/Fitness/src/app/app.routes.server.ts
+++ apps/Fitness/src/app/app.routes.server.ts
@@ -2,6 +2,22 @@ import {RenderMode, ServerRoute} from "@angular/ssr";
 
 export const serverRoutes: ServerRoute[] = [
     {
+        path: ":lang/main/home",
+        renderMode: RenderMode.Server,
+    },
+    {
+        path: ":lang/main/about",
+        renderMode: RenderMode.Prerender,
+    },
+    {
+        path: ":lang/main/classes",
+        renderMode: RenderMode.Server,
+    },
+    {
+        path: ":lang/main/meals",
+        renderMode: RenderMode.Server,
+    },
+    {
         path: "**",
         renderMode: RenderMode.Client,
     },
