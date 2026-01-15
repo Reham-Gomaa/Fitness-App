651656ee1e6ec591ab112503f467156845240c2a
Reham-Gomaa <rehamgomaa686@gmail.com>
2026-01-16 00:08:04 +0200

    fix app name

--- api/index.js
+++ api/index.js
@@ -1,4 +1,2 @@
 // This file bridges Vercel's function call to your Angular SSR handler
-export default import("../dist/apps/FITNESS-APP/server/server.mjs").then(
-    (module) => module.reqHandler
-);
+export default import("../dist/apps/Fitness/server/server.mjs").then((module) => module.reqHandler);
